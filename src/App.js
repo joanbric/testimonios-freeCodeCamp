05bfd70ad3af06a964911d23c9b16b4a7e7ff7a0
Jhon Briones <joanbric@gmail.com>
2022-11-19 15:05:09 +0000

    Testimonios freeCodeCamp, finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonio from "./components/Testimonios";
+import testimonios_db  from "./db/testimonios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="contenedor-principal">
+                <h1>
+                    Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:
+                </h1>
+                <Testimonio 
+                    {...testimonios_db[0]}                
+                />
+                <Testimonio 
+                    {...testimonios_db[1]}
+                />
+                <Testimonio 
+                    {...testimonios_db[2]}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/db/testimonios.js
@@ -0,0 +1,54 @@
+const testimonios_db = [
+    {
+        nombre: "Shawn Wang",
+        pais: "Singapur",
+        imagen: "wang",
+        cargo: "Ingeniero de Software",
+        empresa: "Amazon",
+        testimonio: (
+            <span>
+                Da miedo cambiar de carrera. Solo gané la confianza de que podía
+                programar trabajando a través de los cientos de horas de
+                lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un
+                trabajo de seis cifras como ingeniero de software.{" "}
+                <b>freeCodeCamp cambió mi vida.</b>
+            </span>
+        ),
+    },
+    {
+        nombre: "Sarah Chima",
+        pais: "Nigeria",
+        imagen: "sarah",
+        cargo: "Ingeniera de Software",
+        empresa: "ChatDesk",
+        testimonio: (
+            <span>
+                <b>freeCodeCamp fue la puerta de entrada a mi carrera</b> como
+                desarrollador de software. El plan de estudios bien estructurado
+                llevó mis conocimientos de programación de un nivel de
+                principiante total a un nivel muy seguro. Era todo lo que
+                necesitaba para conseguir mi primer trabajo de desarrollador en
+                una empresa increíble.
+            </span>
+        ),
+    },
+    {
+        nombre: "Emma Bostian",
+        pais: "Suecia",
+        imagen: "emma",
+        cargo: "Imgenira de Software",
+        empresa: "Spotify",
+        testimonio: (
+            <span>
+                Siempre he tenido problemas para aprender JavaScript. He tomado
+                muchos cursos, pero el curso de freeCodeCamp fue el que se
+                quedó. Estudiar JavaScript, así como estructuras de datos y
+                algoritmos en <b>freeCodeCamp me dio las habilidades</b> y la
+                confianza que necesitaba para conseguir el trabajo de mis sueños
+                como ingeniero de software en Spotify.
+            </span>
+        ),
+    },
+];
+
+export default testimonios_db;
